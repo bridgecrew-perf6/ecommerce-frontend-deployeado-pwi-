82914f396fed803cdd04f8cb451a5ab7e1a074b9
camisabatte <camilabelensabatte@gmail.com>
2022-01-29 00:32:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+                                          Proyecto de Entrega Final: Programador Web Inicial
+
+                                                     UTN Regional Buenos Aires
+
+                                                            2021-2022
+
+Estudiante: Sabatté Camila Belén
+
+Profesor a cargo: Pastor Bernal
+
+Este proyecto tiene como fin presentar ante el tutor la aplicación de diversas herramientas aprendidas y estudiadas durante la cursada, para el desarrollo del Frontend de un Sitio Web Ecommerce deployeado en GITHUB PAGES. Se trata de una web para una tienda de calzados con cuatro secciones navegables: el Home, un index de productos generales, el detalle de uno de los productos con formulario para elegir talle y cantidad de compra (Slipper Rafia Amarilla) y el carrito, también desarrollado con sus formularios correspondientes.
+
+HERRAMIENTAS UTILIZADAS PARA CREAR EL PROYECTO
+
+• HTML5
+
+• CSS
+
+• JAVASCRIPT
+
+• VISUAL STUDIO CODE
+
+• GITHUB
+
+• BOX ICONS
+
+• FAVICON
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,56 @@
+const menu = document.querySelector(".menu");
+const navOpen = document.querySelector(".hamburger");
+const navClose = document.querySelector(".close");
+
+const navLeft = menu.getBoundingClientRect().left;
+navOpen.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.add("show");
+    document.body.classList.add("show");
+    navBar.classList.add("show");
+  }
+});
+
+navClose.addEventListener("click", () => {
+  if (navLeft < 0) {
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+    navBar.classList.remove("show");
+  }
+});
+
+// FIXED NAV
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+// SCROLL
+const links = [...document.querySelectorAll(".scroll-link")];
+links.map(link => {
+  if (!link) return;
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href").slice(1);
+
+    const element = document.getElementById(id);
+    const fixNav = navBar.classList.contains("fix-nav");
+    let position = element.offsetTop - navHeight;
+
+    window.scrollTo({
+      top: position,
+      left: 0,
+    });
+
+    navBar.classList.remove("show");
+    menu.classList.remove("show");
+    document.body.classList.remove("show");
+  });
+});
